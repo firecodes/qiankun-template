55c48d492d1d487454c355d3e2b428911b99d9d1
firecodes <384502958@qq.com>
2024-03-17 06:16:11 +0800

    add

---
+++ demo-vue3/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="goSubApp">go react</button>
+    <button @click="goMain">go main</button>
+    <input type="text" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const goSubApp = () => {
+  history.pushState(null, "react-demo", "/react-demo");
+};
+
+const goMain = () => {
+  history.pushState(null, "home", "/home");
+};
+</script>
---
+++ demo-vue3/vue.config.js
@@ -0,0 +1,23 @@
+const isDev = process.env.NODE_ENV === "development";
+module.exports = {
+  publicPath: isDev ? "/" : "/subapp/vue3/",
+  devServer: {
+    port: 3001,
+    // 关闭主机检查，使微应用可以被 fetch
+    disableHostCheck: true,
+    // 配置跨域请求头，解决开发环境的跨域问题
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  configureWebpack: {
+    output: {
+      // 微应用的包名，这里与主应用中注册的微应用名称一致
+      library: "vue3-demo",
+      // 将你的 library 暴露为所有的模块定义下都可运行的方式
+      libraryTarget: "umd",
+      // 按需加载相关，设置为 webpackJsonp_微应用名称 即可
+      jsonpFunction: `webpackJsonp_vue3-demo`,
+    },
+  },
+};
