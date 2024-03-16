27e0f147b75e3c568052658f26b751f7184c08bb
firecodes <384502958@qq.com>
2024-03-17 05:36:37 +0800

    add

--- sub-vue/src/router/index.js
+++ sub-vue/src/router/index.js
@@ -17,8 +17,8 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(
-    // 在乾坤环境下时，将所有的路由地址前加上'/app2'，否则将跳转至主应用的相应页面（eg：跳转至这里的文章详情页面，如果不加'/app2'的话，将跳转至主应用的'/content'页面，如果主应用没有这个页面，将跳转至404页面）
-    qiankunWindow.__POWERED_BY_QIANKUN__ ? '/app2' : '/'
+    // 在乾坤环境下时，将所有的路由地址前加上'/app'，否则将跳转至主应用的相应页面（eg：跳转至这里的文章详情页面，如果不加'/app'的话，将跳转至主应用的'/content'页面，如果主应用没有这个页面，将跳转至404页面）
+    // qiankunWindow.__POWERED_BY_QIANKUN__ ? '/app' : '/'
   ),
   routes
 })
