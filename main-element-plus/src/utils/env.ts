d342e3bc3ebc11df97f5488f97a5881c765cdb27
firecodes <384502958@qq.com>
2024-03-17 16:38:49 +0800

    add

--- main-element-plus/src/utils/env.ts
+++ main-element-plus/src/utils/env.ts
@@ -4,8 +4,8 @@ const env = import.meta.env;
 export const envObject = env;
 export const isDev = env.NODE_ENV === `development`
 export function getPublicPath(): string {
-  return env.VITE_PUBLIC_PATH;
+  return env.VITE_PUBLIC_PATH || '/';
 }
 export function getRootPath(): string {
-  return env.VITE_PUBLIC_PATH;
+  return env.VITE_ROOT_PATH || '/';
 }
