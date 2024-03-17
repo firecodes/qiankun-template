de39f7a1de9243dd805bf730562091ef5992fc74
firecodes <384502958@qq.com>
2024-03-17 16:36:12 +0800

    add

--- main-element-plus/src/micro/apps.ts
+++ main-element-plus/src/micro/apps.ts
@@ -34,63 +34,63 @@ console.log(`main one __process__:`, utils.env)
 const apps: any[] = [
 	// {
 	//   name: `app1`, 
-	//   entry: utils.env.isDev ? `http://localhost:9001/` : `/apps/app1/`,
+	//   entry: utils.env.isDev ? `http://localhost:9001/` : `${utils.env.getRootPath()}app1/`,
 	//   container: `#container`, 
-	//   activeRule: `${utils.env.getPublickPath()}#/app1/`
+	//   activeRule: `${utils.env.getPublicPath()}#/app1/`
 	// },
 	// {
 	//   name: `app2`,
-	//   entry:  utils.env.isDev ? `http://localhost:9002/` : `/apps/app2/`,
+	//   entry:  utils.env.isDev ? `http://localhost:9002/` : `${utils.env.getRootPath()}app2/`,
 	//   container: `#container`,
-	//   activeRule: `${utils.env.getPublickPath()}#/app2/`
+	//   activeRule: `${utils.env.getPublicPath()}#/app2/`
 	// },
 	{
 		name: `sub-html`,
-		entry: utils.env.isDev ? `http://localhost:9003/` : `/apps/sub-html/`,
+		entry: utils.env.isDev ? `http://localhost:9003/` : `${utils.env.getRootPath()}sub-html/`,
 		container: `#container`,
-		activeRule: `${utils.env.getPublickPath()}#/sub-html`
+		activeRule: `${utils.env.getPublicPath()}#/sub-html`
 	},
 	// {
 	//   name: `sub-react`,
-	//   entry: utils.env.isDev ? `http://localhost:9004/`: `/apps/sub-react/`,
+	//   entry: utils.env.isDev ? `http://localhost:9004/`: `${utils.env.getRootPath()}sub-react/`,
 	//   container: `#container`,
-	//   activeRule: `${utils.env.getPublickPath()}#/sub-react/`
+	//   activeRule: `${utils.env.getPublicPath()}#/sub-react/`
 	// },
 	// {
 	// 	name: `sub-vue3-vite`,
-	// 	entry: utils.env.isDev ? `http://localhost:9005/` : `/apps/sub-vue3-vite/`,
+	// 	entry: utils.env.isDev ? `http://localhost:9005/` : `${utils.env.getRootPath()}sub-vue3-vite/`,
 	// 	container: `#container`,
-	// 	activeRule: `${utils.env.getPublickPath()}#/sub-vue3-vite`
+	// 	activeRule: `${utils.env.getPublicPath()}#/sub-vue3-vite`
 	// },
 	// {
 	// 	name: `demo-react`,
-	// 	entry: utils.env.isDev ? `http://localhost:9006/` : `/apps/demo-react/`,
+	// 	entry: utils.env.isDev ? `http://localhost:9006/` : `${utils.env.getRootPath()}demo-react/`,
 	// 	container: `#container`,
-	// 	activeRule: `${utils.env.getPublickPath()}#/demo-react/`
+	// 	activeRule: `${utils.env.getPublicPath()}#/demo-react/`
 	// },
 	// {
 	// 	name: `demo-vue2`,
-	// 	entry: utils.env.isDev ? `http://localhost:9007/` : `/apps/demo-vue2/`,
+	// 	entry: utils.env.isDev ? `http://localhost:9007/` : `${utils.env.getRootPath()}demo-vue2/`,
 	// 	container: `#container`,
-	// 	activeRule: `${utils.env.getPublickPath()}#/demo-vue2/`
+	// 	activeRule: `${utils.env.getPublicPath()}#/demo-vue2/`
 	// },
 	// {
 	// 	name: `demo-vue3`,
-	// 	entry: utils.env.isDev ? `http://localhost:9008/` : `/apps/demo-vue3/`,
+	// 	entry: utils.env.isDev ? `http://localhost:9008/` : `${utils.env.getRootPath()}demo-vue3/`,
 	// 	container: `#container`,
-	// 	activeRule: `${utils.env.getPublickPath()}#/demo-vue3/`
+	// 	activeRule: `${utils.env.getPublicPath()}#/demo-vue3/`
 	// },
 	{
 		name: `sub-vue2`,
-		entry: utils.env.isDev ? `http://localhost:9009/` : `/apps/sub-vue2/`,
+		entry: utils.env.isDev ? `http://localhost:9009/` : `${utils.env.getRootPath()}sub-vue2/`,
 		container: `#container`,
-		activeRule: `${utils.env.getPublickPath()}#/sub-vue2`
+		activeRule: `${utils.env.getPublicPath()}#/sub-vue2`
 	},
 	// {
 	// 	name: `sub-vue3`,
-	// 	entry: utils.env.isDev ? `http://localhost:9010/` : `/apps/sub-vue3/`,
+	// 	entry: utils.env.isDev ? `http://localhost:9010/` : `${utils.env.getRootPath()}sub-vue3/`,
 	// 	container: `#container`,
-	// 	activeRule: `${utils.env.getPublickPath()}#/sub-vue3/`
+	// 	activeRule: `${utils.env.getPublicPath()}#/sub-vue3/`
 	// },
 ];
 export default apps;
--- main-element-plus/src/utils/env.ts
+++ main-element-plus/src/utils/env.ts
@@ -3,6 +3,9 @@ const env = import.meta.env;
 
 export const envObject = env;
 export const isDev = env.NODE_ENV === `development`
-export function getPublickPath(): string {
+export function getPublicPath(): string {
+  return env.VITE_PUBLIC_PATH;
+}
+export function getRootPath(): string {
   return env.VITE_PUBLIC_PATH;
 }
