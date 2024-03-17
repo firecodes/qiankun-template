1d7de70f3580ab4a2a4c57d1cdad1bea9aef0c54
firecodes <384502958@qq.com>
2024-03-18 06:07:05 +0800

    add

--- one/static/AboutView-9b4cfa53.js
+++
@@ -1,1 +0,0 @@
-import{b7 as e,_ as t,$ as s,a4 as o}from"./index-6c37e091.js";const _={},a={class:"about"},c=o("h1",null,"This is an about page",-1),n=[c];function i(r,u){return t(),s("div",a,n)}const l=e(_,[["render",i]]);export{l as default};
