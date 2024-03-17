a310850052b775a76046759945bf93a2c6628487
firecodes <384502958@qq.com>
2024-03-17 16:10:17 +0800

    add

--- sub-html/gulpfile.js
+++ sub-html/gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp')
 const startServer = require('./server.js')
 const fse = require('fs-extra');
+const replace = require('gulp-replace');
+const publicPath = `/qiankun-template/sub-html/` //--path=`/apps/sub-html/`
+// const publicPath = `/apps/sub-html/` //--path=``
 
 function existsFile(dist = 'dist', callback) {
   fse.exists(dist, (exists) => {
@@ -11,11 +14,21 @@ function existsFile(dist = 'dist', callback) {
     }
   })
 }
+gulp.task('update-html-paths', async function () {
+  // 假设你想把所有的"/old-path/"替换为"/new-path/"
+  await gulp.src('./public/**/*.html')
+    .pipe(replace(/<%=publicPath>/g, publicPath))
+    // .pipe(replace('publicPath', publicPath))
+    .pipe(gulp.dest('dist'));
+});
 
 async function copyAssets() {
   existsFile('public', async function () {
     await fse.emptyDir('dist')
-    await gulp.src('./public/**', { nodir: true }).pipe(gulp.dest('dist'))
+    await gulp.src('./public/**/*.html')
+      .pipe(replace(/<%=publicPath>/g, publicPath))
+      .pipe(gulp.dest('dist'));
+    // await gulp.src('./public/**', { nodir: true }).pipe(gulp.dest('dist'))
     await gulp.src('./js/**', { nodir: true }).pipe(gulp.dest('dist/js/'))
     // await gulp
     //   .src('./dist/**', { nodir: true })
@@ -28,7 +41,9 @@ gulp.task('server', gulp.series((done) => {
   done();
 }));
 
-gulp.task('build', gulp.series((done) => {
-  copyAssets()
-  done();
-}));
+gulp.task('build',
+  gulp.series((done) => {
+    copyAssets()
+    done();
+  }) //, 'update-html-paths'
+);
