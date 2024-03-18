826007e33564042acbd727431bfb48469c38ab4d
firecodes <384502958@qq.com>
2024-03-18 18:16:19 +0800

    add

--- auth-web/src/utils/naiveTools.js
+++ auth-web/src/utils/naiveTools.js
@@ -1,7 +1,6 @@
 
 import * as NaiveUI from 'naive-ui'
 import { isNullOrUndef } from '@/utils'
-import { useAppStore } from '@/store'
 
 export function setupMessage(NMessage) {
   class Message {
@@ -94,10 +93,8 @@ export function setupDialog(NDialog) {
 }
 
 export function setupNaiveDiscreteApi() {
-  const appStore = useAppStore()
   const configProviderProps = computed(() => ({
     theme: appStore.isDark ? NaiveUI.darkTheme : undefined,
-    themeOverrides: useAppStore().naiveThemeOverrides,
   }))
   const { message, dialog, notification, loadingBar } = NaiveUI.createDiscreteApi(
     ['message', 'dialog', 'notification', 'loadingBar'],
