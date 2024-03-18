228ed9b1bc43e7a14b2484c9a843a3953eb0115c
firecodes <384502958@qq.com>
2024-03-18 17:23:02 +0800

    add

--- auth-web/build/index.js
+++ auth-web/build/index.js
@@ -1,10 +1,3 @@
-/**********************************
- * @Author: Ronnie Zhang
- * @LastEditor: Ronnie Zhang
- * @LastEditTime: 2023/12/04 22:48:02
- * @Email: zclzone@outlook.com
- * Copyright © 2023 Ronnie Zhang(大脸怪) | https://isme.top
- **********************************/
 
 import { globSync } from 'glob'
 import path from 'path'
