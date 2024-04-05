38de1367409f41d4039aecc1a97968522a76466f
firecodes <384502958@qq.com>
2024-04-06 07:20:14 +0800

    add

--- react-vite-tailwindcss/src/main.jsx
+++ react-vite-tailwindcss/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App.jsx'
-import './index.css'
+import { App } from './App.jsx'
+import '@/styles/app.less'
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+dayjs.extend(duration)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
