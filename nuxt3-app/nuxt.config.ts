2cd52e2a5f183fafd2a94691843d65e3101e2aa8
firecodes <384502958@qq.com>
2024-04-11 19:30:17 +0800

    add

--- nuxt3-app/nuxt.config.ts
+++ nuxt3-app/nuxt.config.ts
@@ -1,4 +1,17 @@
+const path = require('path');
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // nitro: {
+  //   output: {
+  //     publicDir: path.join(__dirname, 'dist')
+  //   }
+  // },
+  app: {
+    baseURL: '/qiankun-template/nuxt-app/',
+    buildAssetsDir: 'nuxt_assets',
+  },
+  // experimental: {
+  //   payloadExtraction: false
+  // },
 })
