0a577041e13916fb55c1a51fd4442a47f96a5288
firecodes <384502958@qq.com>
2024-04-18 09:24:59 +0800

    add

--- vue-vite-threejs/vite.config.js
+++ vue-vite-threejs/vite.config.js
@@ -1,30 +1,45 @@
-import { defineConfig } from 'vite'
+import qiankun from 'vite-plugin-qiankun'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    }
-  },
-  // base: './',
-  build: {
-    // sourcemap: true,
-    commonjsOptions: {
-      strictRequires: true, // 兼容commonjs
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const isBuild = command === 'build'
+  const viteEnv = loadEnv(mode, process.cwd())
+  const { VUE_APP_PORT, VITE_PUBLIC_PATH, VITE_PROXY_TARGET } = viteEnv
+  console.log("viteEnv---", viteEnv)
+  return {
+    base: VITE_PUBLIC_PATH,
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      }
+    },
+    // base: './',
+    build: {
+      outDir: path.resolve(__dirname, "dist"),
+      assetsDir: "static",
+      emptyOutDir: false,
+      target: "esnext", // default，最低为es2015
+      cssCodeSplit: true, // default
+      sourcemap: false, // default
+      // sourcemap: true,
+      commonjsOptions: {
+        strictRequires: true, // 兼容commonjs
+      }
     }
   }
 })
